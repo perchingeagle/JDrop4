c5cbdaf880cecb614886b4ee9f373864ecaba71e
perchingeagle <ysosanya@gmail.com>
2020-12-02 21:53:56 -0500

    Add files via upload

---
+++ Piece.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.cuny.csi.connect4;
+
+/**
+ *
+ * @author yomis
+ */
+public enum Piece {
+    BLUE, RED;
+}
