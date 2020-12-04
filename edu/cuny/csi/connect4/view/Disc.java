0d0a7d3f58dd25916985ca968f8b612acff31a9c
perchingeagle <ysosanya@gmail.com>
2020-12-04 15:41:47 -0500

    Add files via upload
    Documentation added

--- edu/cuny/csi/connect4/view/Disc.java
+++ edu/cuny/csi/connect4/view/Disc.java
@@ -21,11 +21,18 @@ public class Disc extends Circle{
     private final static double TOP_OFFSET = 30;
     private final static double LEFT_OFFSET = 50;
     
+    /**
+     *  Default constructor that is used to created blank cells
+     */
     public Disc(){
         super(RADIUS);
         this.setFill(Color.LAVENDER);
     }
     
+    /**
+     * Constructor used to create pieces for the players 
+     * @param p 
+     */
     public Disc(Piece p){
         super(RADIUS);
         switch(p){
@@ -37,12 +44,24 @@ public class Disc extends Circle{
         }
     }
     
+    /**
+     * Sets the absolute position of the circle on the screen, with the
+     * internal measurement is set in pixel
+     * @param columnIndex The column index or the x-axis 
+     * @return 
+     */
     public double setLocationX(int columnIndex){
         double x = LEFT_OFFSET + (DISPLACEMENT * columnIndex);
         this.setCenterX(x);
         return x;
     }
     
+    /**
+     * Sets the absolute position of the circle on the screen, with the
+     * internal measurement is set in pixel
+     * @param rowIndex
+     * @return 
+     */
     public double setLocationY(int rowIndex){
         // 30 is the top padding
         double y = TOP_OFFSET + (DISPLACEMENT * (rowIndex + 1));
